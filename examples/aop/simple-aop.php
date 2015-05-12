752a89c98541e35d5f20ece735a42aca6ba68919
Marco Pivetta <ocramius@gmail.com>
2015-05-12 04:08:11 +0100

    Splitting bootstrapping logic

--- examples/aop/simple-aop.php
+++ examples/aop/simple-aop.php
@@ -1,37 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-class ApplicationAspectKernel extends \Go\Core\AspectKernel
-{
-    protected function configureAop(\Go\Core\AspectContainer $container)
-    {
-        $container->registerAspect(new CatchTheBuzzes());
-    }
-}
-
-class CatchTheBuzzes implements \Go\Aop\Aspect
-{
-    /**
-     * @Go\Lang\Annotation\Before("execution(public VoodooPhpExamples\*->*(*))")
-     */
-    public function beforeMethodExecution(\Go\Aop\Intercept\MethodInvocation $invocation)
-    {
-        var_dump([
-            'what'   => 'before method call',
-            'object' => $invocation->getThis(),
-            'method' => $invocation->getMethod()->getName(),
-            'args'   => $invocation->getArguments(),
-        ]);
-    }
-}
-
-$applicationAspectKernel = ApplicationAspectKernel::getInstance();
-$applicationAspectKernel->init([
-    'debug'        => true,
-    'cacheDir'     => __DIR__ . '/cache',
-    'includePaths' => [__DIR__ . '/../src/'],
-]);
+require_once __DIR__ . '/simple-bootstrap.php';
 
 echo (new \VoodooPhpExamples\Buzzer())->buzz();
 echo (new \VoodooPhpExamples\Buzzer())->buzz('BEEP');
