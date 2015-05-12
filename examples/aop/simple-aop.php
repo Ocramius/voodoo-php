a8d664a63c1116a5feb27f98dad7aeae202227bb
Marco Pivetta <ocramius@gmail.com>
2015-05-12 04:06:47 +0100

    Simple AOP example

---
+++ examples/aop/simple-aop.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class ApplicationAspectKernel extends \Go\Core\AspectKernel
+{
+    protected function configureAop(\Go\Core\AspectContainer $container)
+    {
+        $container->registerAspect(new CatchTheBuzzes());
+    }
+}
+
+class CatchTheBuzzes implements \Go\Aop\Aspect
+{
+    /**
+     * @Go\Lang\Annotation\Before("execution(public VoodooPhpExamples\*->*(*))")
+     */
+    public function beforeMethodExecution(\Go\Aop\Intercept\MethodInvocation $invocation)
+    {
+        var_dump([
+            'what'   => 'before method call',
+            'object' => $invocation->getThis(),
+            'method' => $invocation->getMethod()->getName(),
+            'args'   => $invocation->getArguments(),
+        ]);
+    }
+}
+
+$applicationAspectKernel = ApplicationAspectKernel::getInstance();
+$applicationAspectKernel->init([
+    'debug'        => true,
+    'cacheDir'     => __DIR__ . '/cache',
+    'includePaths' => [__DIR__ . '/../src/'],
+]);
+
+echo (new \VoodooPhpExamples\Buzzer())->buzz();
+echo (new \VoodooPhpExamples\Buzzer())->buzz('BEEP');
+echo (new \VoodooPhpExamples\Buzzer())->buzz('BOOP');
