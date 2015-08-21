38b096616a00287dd31fc3b3e0dafbebb1095466
Marco Pivetta <ocramius@gmail.com>
2015-08-21 17:27:22 +0100

    Logic to board passengers

---
+++ examples/src/VoodooPhpExamples/Strict/Passenger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace VoodooPhpExamples\Strict;
+
+class Passenger
+{
+    /**
+     * @var string
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     */
+    private $lastName;
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     */
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName  = $lastName;
+    }
+}
--- examples/src/VoodooPhpExamples/Strict/Train.php
+++ examples/src/VoodooPhpExamples/Strict/Train.php
@@ -13,10 +13,25 @@ class Train
     private $wagons;
 
     /**
+     * @var Passenger[][]
+     */
+    private $passengers = [];
+
+    /**
      * @param Wagon[] $wagons
      */
     public function __construct(array $wagons)
     {
         $this->wagons = $wagons;
+    }
+
+    /**
+     * @param Passenger[][] $passengersPerSeatPerWagon
+     *
+     * @return void
+     */
+    public function boardPassengers(array $passengersPerSeatPerWagon)
+    {
+        $this->passengers = $passengersPerSeatPerWagon;
     }
 }
