dd307a79bc33c0cd1ab68d8f1d5f555e591f34f6
Marco Pivetta <ocramius@gmail.com>
2015-08-21 17:27:55 +0100

    Boarding passengers

--- examples/strict-php/generic-train-with-wagons.php
+++ examples/strict-php/generic-train-with-wagons.php
@@ -11,4 +11,19 @@ $train = new \VoodooPhpExamples\Strict\Train(
     ]
 );
 
+$train->boardPassengers([
+    [
+        new \VoodooPhpExamples\Strict\Passenger('Maximilian', 'Berghof'), // @TODO
+        new \VoodooPhpExamples\Strict\Passenger('Andreas', 'Heigl'),
+    ],
+    [
+        new \VoodooPhpExamples\Strict\Passenger('Benjamin', 'Eberlei'),
+        new \VoodooPhpExamples\Strict\Passenger('Kore', 'Normann'), // @TODO check name
+    ],
+    [
+        new \VoodooPhpExamples\Strict\Passenger('Marco', 'Pivetta'),
+        new \VoodooPhpExamples\Strict\Bomb(),
+    ],
+]);
+
 var_dump($train);
